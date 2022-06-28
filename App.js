fec5f23033f16a3fb747993b371a55bf94fa26c9
Renato Areas <rsareas93@gmail.com>
2022-06-28 12:53:05 -0300

    Autenticação de usuários e cadastro de contatos

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
 import Login from "./app/components/screens/Login";
 import Register from "./app/components/screens/Register";
 import Password from "./app/components/screens/Password";
-
+import CadastroContatos from "./app/components/screens/CadastroContatos";
+import ConsultaContatos from "./app/components/screens/ConsultaContatos";
+import EdicaoContatos from "./app/components/screens/EdicaoContatos";
+ 
 const Stack = createNativeStackNavigator();
-
+ 
 //declarando o componente como função
-export default function App(){
-
-  //renderiza o conteudo da tela
-  return(
+export default function App() {
+ 
+  return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="login" component={Login}/>
-        <Stack.Screen name="register" component={Register}/>
-        <Stack.Screen name="password" component={Password}/>
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="register" component={Register} />
+        <Stack.Screen name="password" component={Password} />
+        <Stack.Screen name="cadastro-contatos" component={CadastroContatos} />
+        <Stack.Screen name="consulta-contatos" component={ConsultaContatos} />
+        <Stack.Screen name="edicao-contatos" component={EdicaoContatos} />
       </Stack.Navigator>
     </NavigationContainer>
-
   )
 }
-
